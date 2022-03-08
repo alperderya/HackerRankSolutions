b8023eca32c5dbefeacd38b314456fe1c461d497
Alper Derya <alper.derya@turkcellteknoloji.com.tr>
2022-03-09 01:39:29 +0300

    Java BigDecimal solution

---
+++ src/com/aderya/hackerrank/java/bigdecimal/Solution.java
@@ -0,0 +1,41 @@
+package com.aderya.hackerrank.java.bigdecimal;
+
+import java.math.BigDecimal;
+import java.util.*;
+import java.util.stream.Stream;
+
+class Solution{
+    public static void main(String []args){
+        //Input
+        Scanner sc= new Scanner(System.in);
+        int n=sc.nextInt();
+        String []s=new String[n+2];
+        for(int i=0;i<n;i++){
+            s[i]=sc.next();
+        }
+        sc.close();
+
+        //Write your code here
+        boolean isIterationEnd = false;
+        while(!isIterationEnd){
+            isIterationEnd = true;
+            for(int i=0; i<n-1; i++){
+                BigDecimal first = new BigDecimal(s[i]);
+                BigDecimal second = new BigDecimal(s[i+1]);
+                if(second.compareTo(first)==1){
+                    //swap strings
+                    String temp = s[i];
+                    s[i] = s[i+1];
+                    s[i+1] = temp;
+                    isIterationEnd = false;
+                }
+            }
+        }
+
+        //Output
+        for(int i=0;i<n;i++)
+        {
+            System.out.println(s[i]);
+        }
+    }
+}
