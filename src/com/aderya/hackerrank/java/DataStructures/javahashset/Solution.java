d66b94035585edc7e612b039f470457fc3ef5e93
Alper Derya <alper.derya@gmail.com>
2022-03-19 02:21:37 +0300

    Java Hash Set solution submitted

---
+++ src/com/aderya/hackerrank/java/DataStructures/javahashset/Solution.java
@@ -0,0 +1,27 @@
+package com.aderya.hackerrank.java.DataStructures.javahashset;
+
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int t = s.nextInt();
+        String [] pair_left = new String[t];
+        String [] pair_right = new String[t];
+
+        for (int i = 0; i < t; i++) {
+            pair_left[i] = s.next();
+            pair_right[i] = s.next();
+        }
+
+        //Write your code here
+        Set<String> uniquePairs = new HashSet<>();
+        for (int i = 0; i < t; i++) {
+            uniquePairs.add(pair_left[i] + "#" + pair_right[i]);
+            System.out.println(uniquePairs.size());
+        }
+    }
+}
